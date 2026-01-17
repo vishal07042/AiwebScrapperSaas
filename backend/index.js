913c3fa64fb9b9c67a1d15a5d4a92d611cb44ceb
vishal mehta <professionalprovishal@gmail.com>
2026-01-17 21:18:03 +0530

    __driname

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(process.cwd(), "dist")));
 // });
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(process.cwd(), "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 // Smart Generic scraping endpoint - No predefined schema needed!
